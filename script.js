f39afd154f5d3b1ce65709d1aa7546059a480308
tahid02 <tanzim1463@gmail.com>
2021-02-07 23:34:53 +0600

    tried my level best

--- script.js
+++ script.js
@@ -1,7 +1,101 @@
 
 
 
-document.getElementById('searchBtn').addEventListener('click', function () {
+// document.getElementById('searchBtn').addEventListener('click', function () {
+
+//     const showFoodsParentDiv = document.getElementById('showFoods');
+//     showFoodsParentDiv.innerHTML = '';
+
+//     const inputName = document.getElementById('inputFoodName').value;
+
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputName}`)
+//         .then(response => response.json())
+//         .then(data => {
+
+//             const foodAllInfo = data.meals; // all food information in array
+
+
+//             foodAllInfo.forEach(element => {
+
+//                 const secondParentDiv = document.createElement('div');
+
+//                 // creating  image element and then appending to it's parent div (secondParentDiv)
+//                 const image = document.createElement('img');
+//                 image.src = `${element.strMealThumb}`;
+//                 image.style.width = '300px';
+//                 secondParentDiv.appendChild(image);
+
+//                 // creating  food element and then appending to it's parent div (secondParentDiv)
+//                 const food = document.createElement('h3');
+//                 food.innerText = element.strMeal;
+//                 secondParentDiv.appendChild(food);
+
+//                 // appending sub div element to main div element
+//                 showFoodsParentDiv.appendChild(secondParentDiv);
+
+
+//             });
+
+//         })
+
+//         // if searched food name not found
+//         .catch(err => {
+//             alert('please enter valid name or letter');
+
+//         });
+
+// })
+
+
+// //when clicked on a food image or name
+// document.getElementById('showFoods').addEventListener('click', function (e) {
+
+//     // getting clicked food image
+//     const clickedFoodImage =  e.target.parentNode.childNodes[0].src;
+//     document.getElementById('image').innerHTML = clickedFoodImage;
+
+//     const clickedFoodName = e.target.parentNode.childNodes[1].innerText;
+
+//     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${clickedFoodName}`)
+//         .then(response => response.json())
+//         .then(data => {
+
+//             const allInfo = data.meals[0]; // getting clicked food item object
+
+//             // every array element contain ingredient and measure property 
+//             let array = [ 
+//                 [allInfo.strIngredient1, allInfo.strMeasure1],
+//                 [allInfo.strIngredient2, allInfo.strMeasure2],
+//                 [allInfo.strIngredient3, allInfo.strMeasure3],
+//                 [allInfo.strIngredient4, allInfo.strMeasure4],
+//                 [allInfo.strIngredient5, allInfo.strMeasure5],
+//                 [allInfo.strIngredient6, allInfo.strMeasure6],
+//                 [allInfo.strIngredient7, allInfo.strMeasure7],
+//                 [allInfo.strIngredient8, allInfo.strMeasure8],
+//                 [allInfo.strIngredient9, allInfo.strMeasure9],
+//                 [allInfo.strIngredient10, allInfo.strMeasure10]
+
+//             ];
+
+//             let showIngredient = document.getElementById('showIngredients');
+
+//            showIngredients.innerHTML = `<h2>${clickedFoodName}</h2><br>
+//                                          <h5>Ingredients</h5>`;
+
+
+//             // element[0] is ingredient name and element[1] is its measure                             
+//             array.forEach(element => {
+//                 showIngredients.innerHTML += `<p>  ${element[1]} ${element[0]}  </p>`;
+//             });
+
+
+//         })
+// })
+
+
+
+
+document.getElementById('searchBtn').addEventListener('click', () => {
 
     const showFoodsParentDiv = document.getElementById('showFoods');
     showFoodsParentDiv.innerHTML = '';
@@ -22,7 +116,6 @@ document.getElementById('searchBtn').addEventListener('click', function () {
                 // creating  image element and then appending to it's parent div (secondParentDiv)
                 const image = document.createElement('img');
                 image.src = `${element.strMealThumb}`;
-                image.style.width = '300px';
                 secondParentDiv.appendChild(image);
 
                 // creating  food element and then appending to it's parent div (secondParentDiv)
@@ -40,6 +133,7 @@ document.getElementById('searchBtn').addEventListener('click', function () {
 
         // if searched food name not found
         .catch(err => {
+            document.getElementById('showIngredients').innerHTML = '';
             alert('please enter valid name or letter');
 
         });
@@ -48,22 +142,22 @@ document.getElementById('searchBtn').addEventListener('click', function () {
 
 
 //when clicked on a food image or name
-document.getElementById('showFoods').addEventListener('click', function (e) {
+document.getElementById('showFoods').addEventListener('click', (e) => {
 
-    // getting clicked food image
-    const clickedFoodImage =  e.target.parentNode.childNodes[0].src;
-    document.getElementById('image').innerHTML = clickedFoodImage;
+    const clickedFoodImage = e.target.parentNode.childNodes[0].src;  // getting clicked food image
+    
+    const clickedFoodName = e.target.parentNode.childNodes[1].innerText;  // clicked food name
 
-    const clickedFoodName = e.target.parentNode.childNodes[1].innerText;
 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${clickedFoodName}`)
         .then(response => response.json())
         .then(data => {
-            
+
             const allInfo = data.meals[0]; // getting clicked food item object
-            
+
+
             // every array element contain ingredient and measure property 
-            let array = [ 
+            let allIngredients = [
                 [allInfo.strIngredient1, allInfo.strMeasure1],
                 [allInfo.strIngredient2, allInfo.strMeasure2],
                 [allInfo.strIngredient3, allInfo.strMeasure3],
@@ -74,25 +168,31 @@ document.getElementById('showFoods').addEventListener('click', function (e) {
                 [allInfo.strIngredient8, allInfo.strMeasure8],
                 [allInfo.strIngredient9, allInfo.strMeasure9],
                 [allInfo.strIngredient10, allInfo.strMeasure10]
-
             ];
 
-            let showIngredient = document.getElementById('showIngredients');
-      
-           showIngredients.innerHTML = `<h2>${clickedFoodName}</h2><br>
-                                         <h5>Ingredients</h5>`;
-                                        
-      
+            let showIngredients = document.getElementById('showIngredients');
+
+            showIngredients.innerHTML = `<img src='${clickedFoodImage}' </img>
+                                            <h2>  ${clickedFoodName} </h2> <br>
+                                             <h5> Ingredients </h5>`;
+
+            // show name of ingredients
+            const ingredientItems = document.getElementById('ingredientItems');
+            ingredientItems.innerHTML = '';
+
             // element[0] is ingredient name and element[1] is its measure                             
-            array.forEach(element => {
-                showIngredients.innerHTML += `<p>  ${element[1]} ${element[0]}  </p>`;
+            allIngredients.forEach(element => {
+                if (element[0] !== '' && element[1] !== '') {
+                    ingredientItems.innerHTML += `<li>  ${element[1]} ${element[0]}  </li>`;
+                }
+
+
+
             });
 
 
-        })
+        });
 })
-
-
 
 
 
